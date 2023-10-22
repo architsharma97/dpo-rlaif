cb59123a62aea8b2a1ab99f72db0eecf69c3f00f
architsharma97 <archit97sharma@gmail.com>
2023-10-22 06:15:05 +0000

    removing claude 2 evals, better printing

--- eval_ckpt.sh
+++ eval_ckpt.sh
@@ -14,8 +14,8 @@ echo "starting alpaca evaluation" > $2/eval_log.txt
 if [ $annotator == "all" ]; then
     mkdir -p $2/gpt4
     screen -dmS eval_gpt4 bash -c "conda run -n rlhf --no-capture-output alpaca_eval --model_outputs $2/alpaca_eval_temp$temperature.json --annotators_config gpt4 --name $3 --output_path $2/gpt4"
-    mkdir -p $2/claude_2
-    screen -dmS eval_c2 bash -c "conda run -n rlhf --no-capture-output alpaca_eval --model_outputs $2/alpaca_eval_temp$temperature.json --annotators_config claude_2 --name $3 --output_path $2/claude_2"
+    # mkdir -p $2/claude_2
+    # screen -dmS eval_c2 bash -c "conda run -n rlhf --no-capture-output alpaca_eval --model_outputs $2/alpaca_eval_temp$temperature.json --annotators_config claude_2 --name $3 --output_path $2/claude_2"
     mkdir -p $2/claude_1
     screen -dmS eval_c1 bash -c "conda run -n rlhf --no-capture-output alpaca_eval --model_outputs $2/alpaca_eval_temp$temperature.json --annotators_config claude --name $3 --output_path $2/claude_1"
 else
